7e90483d38d7188b6604862706b2c9eeea86a475
Gabriel Gomes <glga@gft.com>
2018-08-27 21:48:37 -0300

    (lista notas) criando listas com RecyclerView

--- app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
@@ -1,9 +1,17 @@
 package br.com.alura.ceep.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
 
 import br.com.alura.ceep.R;
+import br.com.alura.ceep.dao.NotaDAO;
+import br.com.alura.ceep.model.Nota;
+import br.com.alura.ceep.ui.recyclerview.adapter.ListaNotasAdapter;
 
 public class ListaNotasActivity extends AppCompatActivity {
 
@@ -11,5 +19,21 @@ public class ListaNotasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
+
+        RecyclerView listaNotas = findViewById(R.id.lista_notas_recyclerview);
+
+        NotaDAO notaDAO = new NotaDAO();
+
+        for(int i = 0; i <= 10000; i++) {
+            notaDAO.insere(new Nota("Título " + i, "Descrição " + i));
+        }
+
+        List<Nota> todasNotas = notaDAO.todos();
+
+        listaNotas.setAdapter(new ListaNotasAdapter(this, todasNotas));
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        listaNotas.setLayoutManager(linearLayoutManager);
+
+
     }
 }
